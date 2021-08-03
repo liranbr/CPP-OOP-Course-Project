86f28763051feccce8c3a81a46ddad589bca1bf3
Numachka <nahumkletkin@gmail.com>
2021-08-03 16:16:46 +0300

    Fixed cpp files.

--- CCrewMember.cpp
+++ CCrewMember.cpp
@@ -1,42 +1,36 @@
 #include "CrewMember.h"
- 
-class CCrewMember {
-private:
-	char* crewMemberName;
-	int airMinutes;
-public:
-	CCrewMember(const char* crewMemberName, int airMinutes = 0) {
-		strcpy(this->crewMemberName, crewMemberName);
-		this->airMinutes = airMinutes;
-	}
-	bool UpdateMinutes(int additionalAirMinutes) {
-		if (additionalAirMinutes > 0) {
-			airMinutes += additionalAirMinutes;
-			return true;
-		}
-		return false;
-	}
-	int GetMinutes() {
-		return airMinutes;
-	}
-	void SetCrewMemberName(const char* newCrewMemberName) {
-		strcpy(this->crewMemberName, newCrewMemberName);
-	}
-	char* GetCrewMemberName() {
-		return strdup(crewMemberName);
-	}
-	void Print(ostream& outstream) {
-		outstream << "Crewmember " << crewMemberName << "minutes" << airMinutes;
-	}
-	bool IsEqual(CCrewMember otherCrewMember) {
-		return this->crewMemberName == otherCrewMember.GetCrewMemberName();
-		/*
-		if (strlen(crewMemberName) != strlen(otherCrewMemberName.GetCrewMemberName()))
-			return false;
-		for (int i = 0; i < strlen(crewMemberName); i++)
-			if (*(crewMemberName + i) != *(otherCrewMemberName + i))
-				return false;
+
+CCrewMember::CCrewMember(const char* crewMemberName, int airMinutes = 0) {
+	strcpy_s(this->crewMemberName, strlen(crewMemberName), crewMemberName);
+	this->airMinutes = airMinutes;
+}
+bool CCrewMember::UpdateMinutes(int additionalAirMinutes) {
+	if (additionalAirMinutes > 0) {
+		airMinutes += additionalAirMinutes;
 		return true;
-		*/
 	}
-};
+	return false;
+}
+int CCrewMember::GetMinutes() {
+	return airMinutes;
+}
+void CCrewMember::SetCrewMemberName(const char* newCrewMemberName) {
+	strcpy_s(this->crewMemberName, strlen(newCrewMemberName), newCrewMemberName);
+}
+char* CCrewMember::GetCrewMemberName() {
+	return _strdup(crewMemberName);
+}
+void CCrewMember::Print(ostream& outstream) {
+	outstream << "Crewmember " << crewMemberName << "minutes" << airMinutes;
+}
+bool CCrewMember::IsEqual(CCrewMember otherCrewMember) {
+	return this->crewMemberName == otherCrewMember.GetCrewMemberName();
+	/*
+	if (strlen(crewMemberName) != strlen(otherCrewMemberName.GetCrewMemberName()))
+		return false;
+	for (int i = 0; i < strlen(crewMemberName); i++)
+		if (*(crewMemberName + i) != *(otherCrewMemberName + i))
+			return false;
+	return true;
+	*/
+}
--- CFlightInfo.cpp
+++ CFlightInfo.cpp
@@ -1,41 +1,38 @@
 #include "FlightInfo.h"
 
-class CFlightInfo {
-private:
-	int flightNum;
-	char destination[MAX];
-	int duration;
-	int distance;
-public:
-	CFlightInfo(const char destination[MAX], int flightNum, int duration, int distance) {
-		strcpy(this->destination, destination);
-		this->flightNum = flightNum;
-		this->duration = duration;
-		this->distance = distance;
-	}
-
-	bool IsEqual(int otherFlightNum) {
-		return this->flightNum == otherFlightNum;
-	}
-	void Print(ostream& outstream) {
-		outstream << "Flight Info dest: " << destination << " Number " << flightNum << " minutes " << duration << " KM " << distance;
-	}
-
-	int GetFlightNum() { 
-		return flightNum;}
-	char* GetDest() {
-		return strdup(destination);}
-	int GetDuration() {
-		return duration;}
-	int GetDistance() {
-		return distance;}
-
-	void SetFlightNum(int flightNum) { 
-		this->flightNum = flightNum;}
-	void SetDest(const char destination[MAX]) {
-		strcpy(this->destination, destination);}
-	void SetDuration(int duration) { 
-		this->duration = duration;}
-	void SetDistance(int distance) {
-		this->distance = distance;}
-};
+CFlightInfo::CFlightInfo(const char destination[MAX], int flightNum, int duration, int distance) {
+	strcpy_s(this->destination, strlen(destination), destination);
+	this->flightNum = flightNum;
+	this->duration = duration;
+	this->distance = distance;
+}
+bool CFlightInfo::IsEqual(int otherFlightNum) {
+	return this->flightNum == otherFlightNum;
+}
+void CFlightInfo::Print(ostream& outstream) {
+	outstream << "Flight Info dest: " << destination << " Number " << flightNum << " minutes " << duration << " KM " << distance;
+}
+int CFlightInfo::GetFlightNum() {
+	return flightNum;
+}
+char* CFlightInfo::GetDest() {
+	return _strdup(destination);
+}
+int CFlightInfo::GetDuration() {
+	return duration;
+}
+int CFlightInfo::GetDistance() {
+	return distance;
+}
+void CFlightInfo::SetFlightNum(int flightNum) {
+	this->flightNum = flightNum;
+}
+void CFlightInfo::SetDest(const char destination[MAX]) {
+	strcpy_s(this->destination, strlen(destination), destination);
+}
+void CFlightInfo::SetDuration(int duration) {
+	this->duration = duration;
+}
+void CFlightInfo::SetDistance(int distance) {
+	this->distance = distance;
+}
